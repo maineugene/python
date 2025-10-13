abf3437ff58fae62e8eb28a1493375f78c09285f
evgeniy <eugenio7002@gmail.com>
2025-10-13 14:38:31 +0300

    deleting files

--- task_6.py
+++
@@ -1,8 +0,0 @@
-def gas_amount(pressure, temperature, volume):
-    ''' Задача 6. Уравнение состояния идеального газа '''
-    R = 8.31
-    return pressure * volume / (R * temperature)
-# pv = nRT n=pv/RT
-pressure , temperature, volume = float(input("введите давление(в Паскалях):")),float(input("введите температуру (в Кельвинах):")), float(input("введите объем(в м^3):"))
-print("количество газа:",gas_amount(pressure , temperature, volume)," моль")
-
